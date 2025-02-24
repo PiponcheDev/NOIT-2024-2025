3133a0d9cee02e2e24fc7cd6b97d0a88000965eb
BorisPanayotov <bothegamer@gmail.com>
2025-02-24 15:36:44 +0200

    Database changes

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Време на генериране: 21 фев 2025 в 05:50
+-- Време на генериране: 24 фев 2025 в 14:35
 -- Версия на сървъра: 10.4.32-MariaDB
 -- Версия на PHP: 8.2.12
 
@@ -32,20 +32,14 @@ CREATE TABLE `card` (
   `cardToken` varchar(10000) NOT NULL,
   `cardType` char(1) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `purchaseDate` timestamp NULL DEFAULT NULL
+  `purchaseDate` timestamp NULL DEFAULT NULL,
+  `ticketType` int(25) DEFAULT NULL,
+  `purchaseDate_ticket` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Схема на данните от таблица `card`
 --
-
-INSERT INTO `card` (`id`, `cardToken`, `cardType`, `user_id`, `purchaseDate`) VALUES
-(74, '9d690960d9495cdef89c02638f37fd0c', 'R', 74, '2025-02-17 16:54:59'),
-(75, 'eba295a905a1d4222c67e9f50384aac8', 'S', 75, '2025-02-18 18:04:59'),
-(76, '378132d6d51c7700aa1add2ab85a18cc', 'R', 76, '2025-02-18 17:41:33'),
-(77, 'cf5af2f05536b43c0f9d3eb2f598b097', '', 77, NULL),
-(78, '95b65942a034c55e10495131c349451d', '', 78, NULL),
-(79, 'b750591378b797c3e465430660f5e0e6', '', 79, NULL);
 
 -- --------------------------------------------------------
 
@@ -64,13 +58,7 @@ CREATE TABLE `user` (
 -- Схема на данните от таблица `user`
 --
 
-INSERT INTO `user` (`id`, `email`, `username`, `password`) VALUES
-(74, 'a@gmail.com', 'a', '$2y$10$hpaXYhOJp70/s5nc7znV0eWiOAEyC1SlxPjXy603B0fzrcSMM7jVG'),
-(75, 'b@gmail.com', 'b', '$2y$10$cPy6WrxVIEUmOlCMt/zW0eDJGrZMmqJUDM3.XlMFdbYcr9b9rlWOy'),
-(76, 'gfg@gmail.com', 'gfd', '$2y$10$Rixgmgwjqca3x5POXpnkKuIfPgCtWPdY5CDPru5sRBKjE0WOqJ1bS'),
-(77, 'g@gmail.com', 'g', '$2y$10$uRA7tDoFV/X9EL3elzqEsuP/Ryw15UlDw8/hH9f.AjzYOqrhY.x9S'),
-(78, 'fd@gmail.com', 'fd', '$2y$10$JxzymUpWFbhlEZDlmq6Y8eMGOSi8OHFnMDgFd72LZ7QF9HPm7FGyW'),
-(79, 'f@gmail.com', 'f', '$2y$10$1zywNgf7UXuSjGYn0NeCLuKIgcvgMtQ1k6FDH.92PaXG2Rf9g/HlS');
+
 
 --
 -- Indexes for dumped tables
@@ -97,13 +85,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `card`
 --
 ALTER TABLE `card`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
 
 --
 -- Ограничения за дъмпнати таблици
